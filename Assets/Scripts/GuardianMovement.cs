f995c5cdb35fdc62b1e0a417af5583715851e1ff
Khatai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2025-05-28 19:35:42 +0200

    lighting and enemy AI

--- Assets/Scripts/GuardianMovement.cs
+++ Assets/Scripts/GuardianMovement.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
 using static UnityEngine.GraphicsBuffer;
 
 public class GuardianMovement : MonoBehaviour
 {
     public Transform[] waypoints; //array of waypoints(enemies' path)
     private int waypointIndex = 0; //this the current waypoiny enemy is moving to
-    public float speed = 3.0f;  //enemy movement speed
+    //public float speed = 3.0f;  //enemy movement speed
 
+
+    private NavMeshAgent agent;
     public float rotationSpeed = 20.0f; //rotation speed of the enemy robot
+
+    public Transform target; //player
+
+    private float lookRadius = 5f;
+
 
     void Start()
     {
         // fill the waypaoints array. add waypoints
+        agent = GetComponent<NavMeshAgent>();
+        //currentTarget = waypoints[1];
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (waypointIndex < waypoints.Length)
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+        if (distance < lookRadius) {
+            FacePlayer();
+            agent.SetDestination(target.transform.position);
+
+        }
+        if (distance > lookRadius) {
+            FaceWp();
+            if (!agent.pathPending && agent.remainingDistance < 5)
+            {
+                waypointIndex = waypointIndex == 0 ? 1 : 0;
+                Move();
+            }
+
+
+        }
+        
+       /* if (waypointIndex < waypoints.Length)
         {
 
             transform.position = Vector3.MoveTowards(transform.position,
@@ -42,12 +70,12 @@ public class GuardianMovement : MonoBehaviour
 
 
 
-            if (Vector3.Distance(transform.position, waypoints[waypointIndex].position)
+            if (Vector3.Distance(this.transform.position, waypoints[waypointIndex].position)
                 < 0.1f && waypointIndex != 1)
             {
                 waypointIndex++;
             }
-            else if (Vector3.Distance(transform.position, waypoints[waypointIndex].position)
+            else if (Vector3.Distance(this.transform.position, waypoints[waypointIndex].position)
                 < 0.1f && waypointIndex == 1) {
                 waypointIndex--;
             }
@@ -56,6 +84,38 @@ public class GuardianMovement : MonoBehaviour
                 //enemy will shoot tower
             }
 
+        }*/
+    }
+
+    void FaceWp() {
+        Vector3 direction = (waypoints[waypointIndex].transform.position - this.transform.position).normalized;
+        direction.y = 0;
+
+        if (direction.magnitude > 0.01f)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
         }
+    }
+
+    void FacePlayer()
+    {
+        Vector3 direction = (target.transform.position - this.transform.position).normalized;
+        direction.y = 0;
+
+        if (direction.magnitude > 0.01f)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 20);
+        }
+    }
+
+
+    void Move() {
+        if(agent != null && waypoints[waypointIndex] != null ){
+            agent.SetDestination(waypoints[waypointIndex].transform.position);
+
+        }
+    
     }
 }
